c574eda5afaad3fbd7a2db659ccd083a6c88b8c8
yash khairnar <yash khairnar>
2017-03-13 13:17:31 +0530

    all views added

--- angular/controllers/app.js
+++ angular/controllers/app.js
@@ -1,4 +1,4 @@
-var body={};
+
 var myApp=angular.module('myApp',[]);
 myApp.controller('roundController',['$http','apiService',function($http,apiService){
 
@@ -56,7 +56,7 @@ var main=this;
 	this.tieMatches=0;
 	
 	function getTotalStats(Team){
-	
+	    
 		var score=matchWin=lostMatch=tieMatch=0;
 		for(var index in team){
 			$.each(team[index].matches,function(i,v){
@@ -101,7 +101,7 @@ var main=this;
 			
 		},function failure(){
 			alert("Some error has occured while loading API")
-		})
+		});
 	
 	this.getTeamScore=function(team){
 		getTotalStats(team);
@@ -109,3 +109,55 @@ var main=this;
 	}
 
 }])
+myApp.controller("getInfo",function(){
+	this.match={};
+	this.teams=[];
+	this.teams1=[];
+	this.rounds=[];
+	this.team1="TEAM1";
+	this.team2="TEAM2";
+	this.round=0;
+
+	var main=this;
+	
+	this.getInfo=function(round,team1,team2){
+
+		for(var i in team[round].matches){
+			if(team1==team[round].matches[i].team1.name && team2==team[round].matches[i].team2.name){
+					main.match=team[round].matches[i];
+					
+					
+					
+			}
+		}
+		
+	}
+	this.getRounds=function(){
+		for(var i in team)
+			main.rounds.push(i);
+		
+	}
+	this.getTeam1=function(round){
+	
+		for(var i in team[round].matches){
+			
+			
+				main.teams.push(team[round].matches[i].team1.name);
+				
+			
+		}
+
+	}
+	this.getTeam2=function(round,team1){
+		
+			for(var index in team[round].matches)
+			{  	
+				if(team[round].matches[index].team1.name==team1)
+				main.teams1.push(team[round].matches[index].team2.name);
+			}
+		
+	}
+
+
+})
+	
--- angular/controllers/route.js
+++ angular/controllers/route.js
@@ -1,19 +1,26 @@
 $(".Rounds").show();//change it to show afterwards
 $(".teamStats").hide();
+$(".singleMatchDetails").hide();
 $("#RoundTab").click(function(){
-	$(".teamStats").hide();
+	
 	$(".Rounds").show();
-
+	$(".singleMatchDetails").hide();
+	$(".teamStats").hide();
+	
 
 })
 $("#statsTab").click(function(){
 	$(".Rounds").hide();
 	//complete this function here
+	$(".singleMatchDetails").show();
+	$(".teamStats").hide();
 
 })
 
 $("#teamStatsTab").click(function(){
 		$(".Rounds").hide();
 		$(".teamStats").show();
+		$(".singleMatchDetails").hide();
+
 	//complete this function here
 })
